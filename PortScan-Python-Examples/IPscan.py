aa940117af73c13638e65e1c032819d538372d30
mowlkmorok <65051191+mowlkmorok@users.noreply.github.com>
2021-04-25 06:57:45 -0400

    Update IPscan.py

--- PortScan-Python-Examples/IPscan.py
+++ PortScan-Python-Examples/IPscan.py
@@ -1,6 +1,6 @@
 import subprocess
 
-for a in range(100, 105):  #Range IP
+for a in range(100, 105):  # IP Range
     ips = "192.168.0." + str(a)
     x = subprocess.call(['ping', '-n', '1', ips ])
     print(x)
